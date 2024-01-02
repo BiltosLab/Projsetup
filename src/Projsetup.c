90e882b1ef37d354cd3427d463e221350c3990cb
BiltosLab <mrlaith44@gmail.com>
2024-01-02 03:09:59 +0300

    Fixed up all the issues with strings all files work as it should now

--- src/Projsetup.c
+++ src/Projsetup.c
@@ -1,31 +1,33 @@
 #include "Projsetup.h"
 
-
-
- void mkdir_cd(const char* name) 
+void mkdir_cd(const char *name)
 {
-       mkdir(name,0777);
-       chdir(name);
+    mkdir(name, 0777);
+    chdir(name);
 }
-void filecreate(char* filename,const char* filecontent)
+void filecreate(char *filename, const char *filecontent)
 {
     FILE *fp;
-    fp = fopen(filename,"w");
-    fprintf(fp, "%s",filecontent);
+    fp = fopen(filename, "w");
+    fprintf(fp, "%s", filecontent);
     fclose(fp);
 }
 
-char* projmake(char* projname,char lang)
+char *projmake(char *projname, char lang)
 {
-    mkdir_cd(projname,0777); // Make the root dir of the project
+    mkdir_cd(projname); // Make the root dir of the project
+    int result = system("git init");
+
     // Determine the lengths of the strings
-    char makefile[50];
+    //int sizeofmake = sizeof(makeftop)+sizeof(makefbot);
+    char makefile[300];
     size_t filenameLength = strlen(projname);
-    size_t extensionLength = 4;  // Assuming ".cpp" or ".h" can be expanded later if i add more langs other than those 2.
-    char *cmainFilename = (char *)malloc(filenameLength + extensionLength + 1);  // +1 for the null terminator otherwise we get cut off names and we dont want that.
+    size_t extensionLength = 4;                                                 // Assuming ".cpp" or ".h" can be expanded later if i add more langs other than those 2.
+    char *cmainFilename = (char *)malloc(filenameLength + extensionLength + 1); // +1 for the null terminator otherwise we get cut off names and we dont want that.
     char *hFilename = (char *)malloc(filenameLength + extensionLength + 1);
     // Check if memory allocation is successful
-    if (cmainFilename == NULL || hFilename == NULL) {
+    if (cmainFilename == NULL || hFilename == NULL)
+    {
         perror("Memory allocation error");
         exit(1);
     }
@@ -40,43 +42,45 @@ char* projmake(char* projname,char lang)
     {
         strcat(cmainFilename, cppext);
     }
-    
+
     strcpy(hFilename, projname);
     strcat(hFilename, headerext);
-    snprintf(makefile,sizeof(makefile),"%sgcc %s -o %s\n%s",makeftop,cmainFilename,projname,makefbot); // idk what a better way to implement this but rn this should do.
-    filecreate("Makefile",makefile);
-    filecreate("README.md",readme);
-    filecreate(".gitignore",gitignore);
-    mkdir("build",0777);
+    snprintf(makefile, sizeof(makefile), "%s\tgcc ./src/%s -o ./build/%s\n%s", makeftop, cmainFilename, projname, makefbot); // idk what a better way to implement this but rn this should do.
+    filecreate("Makefile", makefile);
+    filecreate("README.md", readme);
+    filecreate(".gitignore", gitignore);
+    mkdir("build", 0777);
     mkdir_cd("src");
     if (lang == 'c')
     {
-        filecreate(cmainFilename,cfile);
+        filecreate(cmainFilename, cfile);
     }
     else if (lang == 'a')
     {
-        filecreate(cmainFilename,cppfile);
+        filecreate(cmainFilename, cppfile);
     }
 
-    filecreate(hFilename,header);
+    filecreate(hFilename, header);
     free(cmainFilename);
     free(hFilename);
-    
-    
+
+    if (result == 0) {
+        printf("Success! everything should be setup now\n");
+    } else {
+        printf("Git init failed make sure you have git installed (command exit code %d).\n", result);
+    }
 }
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     char projectname[NAMELEN];
     printf("Enter name of project:!\n");
     fgets(projectname, sizeof(projectname), stdin);
-    if (projectname[strlen(projectname) - 1] == '\n') {
+    if (projectname[strlen(projectname) - 1] == '\n')
+    {
         projectname[strlen(projectname) - 1] = '\0';
     }
-    projmake(projectname,'c');
-    
-
-
+    projmake(projectname, 'c');
 
     return 0;
 }
